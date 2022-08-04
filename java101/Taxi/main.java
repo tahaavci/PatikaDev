4140d36c7b0121d51110bdecb54bcfc0211673d9
Hp <hp@taha.com>
2022-08-05 02:45:47 +0300

    taxi added.

---
+++ java101/Taxi/main.java
@@ -0,0 +1,31 @@
+package java101.Taxi;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Mesafe giriniz :");
+            double km = sc.nextDouble();
+
+            System.out.println("Ücret : "+taxi(km));
+
+
+    }
+
+
+
+    static double taxi(double km){
+
+        double cost = 10+km*2.2;
+
+        if(cost<20)
+        return 20;
+        else 
+        return cost;
+    }
+
+    
+}
